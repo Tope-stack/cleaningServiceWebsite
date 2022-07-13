bf3d7041f2db8c34e6e0b4bfce64d663f434c179
Tope-stack <yusufftope70@gmail.com>
2022-07-13 17:18:22 +0100

    updated css,js,html

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ const headerBg = () => {
         if(this.scrollY > 0){
             header.classList.add("bg-reveal");
         }
+        else{
+            header.classList.remove("bg-reveal");
+        }
     });
-}
+ }
 headerBg()
